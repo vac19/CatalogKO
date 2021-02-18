bb04fb94c9787f0477c01ce66e4a1c15d509391f
Vashishtha Chauhan <76933015+vac19@users.noreply.github.com>
2021-02-18 16:12:25 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Advertisment Module registration.
+ * @category  Category KnockOut
+ * @package   Cat_Ko
+ * @author    vashishtha
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Cat_Ko',
+    __DIR__
+);
